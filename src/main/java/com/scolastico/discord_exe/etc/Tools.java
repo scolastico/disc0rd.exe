b233052b3a46d8b0dcca993e9246dc76c0efb512
Joschua Becker <joschuabeckerprivat@gmail.com>
2019-12-30 09:44:39 +0100

    Update Tools.java

--- src/main/java/com/scolastico/discord_exe/etc/Tools.java
+++ src/main/java/com/scolastico/discord_exe/etc/Tools.java
@@ -1,5 +1,7 @@
 package com.scolastico.discord_exe.etc;
 
+import java.util.concurrent.TimeUnit;
+
 public class Tools {
 
     private Tools() {}
@@ -15,6 +17,25 @@ public class Tools {
         for (int tmp = 0; times > tmp; tmp++) {
             System.out.println("");
         }
+    }
+
+    private Boolean _isShowingLoadingAnimation = false;
+    public Boolean isShowingLoadingAnimation() {
+        return _isShowingLoadingAnimation;
+    }
+
+    public void asyncLoadingAnimationWhileWaitingResult(Runnable function) {
+        _isShowingLoadingAnimation = true;
+        Thread thread = new Thread(function);
+        thread.start();
+        while(thread.isAlive()) {
+            System.out.print(".");
+            try {
+                TimeUnit.MILLISECONDS.sleep(500);
+            } catch (InterruptedException ignored) {}
+        }
+        System.out.println(" [OK]");
+        _isShowingLoadingAnimation = false;
     }
 
 }
