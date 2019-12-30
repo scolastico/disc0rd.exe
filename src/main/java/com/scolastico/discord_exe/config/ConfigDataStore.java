af7776587672380000d93b2d8af52a2d27737749
Joschua Becker <joschuabeckerprivat@gmail.com>
2019-12-30 09:44:20 +0100

    adding config handler

---
+++ src/main/java/com/scolastico/discord_exe/config/ConfigDataStore.java
@@ -0,0 +1,50 @@
+package com.scolastico.discord_exe.config;
+
+public class ConfigDataStore {
+
+    private String mysql_server = "localhost:3306";
+    private String mysql_user = "username";
+    private String mysql_pass = "password";
+    private String mysql_database = "database";
+    private String mysql_prefix = "disc0rd_";
+
+    public String getMysql_server() {
+        return mysql_server;
+    }
+
+    public String getMysql_database() {
+        return mysql_database;
+    }
+
+    public String getMysql_pass() {
+        return mysql_pass;
+    }
+
+    public String getMysql_prefix() {
+        return mysql_prefix;
+    }
+
+    public String getMysql_user() {
+        return mysql_user;
+    }
+
+    public void setMysql_server(String mysql_server) {
+        this.mysql_server = mysql_server;
+    }
+
+    public void setMysql_database(String mysql_database) {
+        this.mysql_database = mysql_database;
+    }
+
+    public void setMysql_pass(String mysql_pass) {
+        this.mysql_pass = mysql_pass;
+    }
+
+    public void setMysql_prefix(String mysql_prefix) {
+        this.mysql_prefix = mysql_prefix;
+    }
+
+    public void setMysql_user(String mysql_user) {
+        this.mysql_user = mysql_user;
+    }
+}
---
+++ src/main/java/com/scolastico/discord_exe/config/ConfigHandler.java
@@ -0,0 +1,66 @@
+package com.scolastico.discord_exe.config;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+import java.io.*;
+
+public class ConfigHandler {
+
+    private Object _configObject;
+    private String _filename;
+    private File _file;
+    private Gson _gson;
+
+    public ConfigHandler(Object configObject, String filename, Boolean throwErrorOnCreation) throws Exception {
+
+        _filename = filename;
+        _file = new File(filename);
+        _gson = new GsonBuilder().setPrettyPrinting().create();
+
+        if (_file.exists()) {
+            _configObject = _gson.fromJson(new FileReader(_file), configObject.getClass());
+        } else {
+            writeStringToFile(_gson.toJson(configObject));
+            throw new Exception("Config created! Please edit it and then restart!");
+        }
+
+    }
+
+    public ConfigHandler(Object configObject, String filename) throws Exception {
+
+        _filename = filename;
+        _file = new File(filename);
+        _gson = new GsonBuilder().setPrettyPrinting().create();
+
+        if (_file.exists()) {
+            _configObject = _gson.fromJson(new FileReader(_file), configObject.getClass());
+        } else {
+            writeStringToFile(_gson.toJson(configObject));
+            _configObject = configObject;
+        }
+
+    }
+
+    private void writeStringToFile(String string) throws IOException {
+        BufferedWriter writer = new BufferedWriter(new FileWriter(_file));
+        writer.write(string);
+        writer.close();
+    }
+
+    public Object getConfigObject() {
+        return _configObject;
+    }
+
+    public void setConfigObject(Object configObject) {
+        this._configObject = configObject;
+    }
+
+    public void saveConfigObject() throws IOException {
+        _gson.toJson(_configObject, new FileWriter(_file));
+    }
+
+    public String getFilename() {
+        return _filename;
+    }
+}
