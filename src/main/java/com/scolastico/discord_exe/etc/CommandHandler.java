1520fa1acd286d00c00347b3b597cca5c8b6a96e
Joschua Becker <joschuabeckerprivat@gmail.com>
2019-12-30 22:42:07 +0100

    Update CommandHandler.java

--- src/main/java/com/scolastico/discord_exe/etc/CommandHandler.java
+++ src/main/java/com/scolastico/discord_exe/etc/CommandHandler.java
@@ -1,7 +1,10 @@
 package com.scolastico.discord_exe.etc;
 
+import net.dv8tion.jda.api.JDA;
+import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
+
 public interface CommandHandler {
 
-    public boolean respondToCommand(String cmd, String[] args);
+    public boolean respondToCommand(String cmd, String[] args, JDA jda, MessageReceivedEvent event, long senderId, long serverId);
 
 }
