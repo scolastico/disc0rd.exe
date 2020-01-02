3666f47b3a5d07499e7ebe7acb630f41d8f52698
Joschua Becker <joschuabeckerprivat@gmail.com>
2020-01-02 07:56:55 +0100

    Update ServerSettings.java

--- src/main/java/com/scolastico/discord_exe/mysql/ServerSettings.java
+++ src/main/java/com/scolastico/discord_exe/mysql/ServerSettings.java
@@ -1,8 +1,13 @@
 package com.scolastico.discord_exe.mysql;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+
 public class ServerSettings {
 
     private boolean isSpecialServer = false;
+
+    private ArrayList<HashMap<String, String>> shortCuts = new ArrayList<HashMap<String, String>>();
 
     public boolean isSpecialServer() {
         return isSpecialServer;
@@ -12,4 +17,11 @@ public class ServerSettings {
         isSpecialServer = specialServer;
     }
 
+    public ArrayList<HashMap<String, String>> getShortCuts() {
+        return shortCuts;
+    }
+
+    public void setShortCuts(ArrayList<HashMap<String, String>> shortCuts) {
+        this.shortCuts = shortCuts;
+    }
 }
