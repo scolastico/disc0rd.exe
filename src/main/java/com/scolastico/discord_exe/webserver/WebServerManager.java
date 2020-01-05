2a7b6552bb676238b69c47349de96af756aa5d39
Joschua Becker <joschuabeckerprivat@gmail.com>
2020-01-05 12:27:09 +0100

    Update WebServerManager.java

--- src/main/java/com/scolastico/discord_exe/webserver/WebServerManager.java
+++ src/main/java/com/scolastico/discord_exe/webserver/WebServerManager.java
@@ -19,6 +19,7 @@ public class WebServerManager implements HttpHandler {
     private static WebServerManager instance = null;
     private HttpServer httpServer;
     private HashMap<WebHandler, ArrayList<String>> webHandlers = new HashMap<WebHandler, ArrayList<String>>();
+    private static final String error404 = "<div id=\"main\"><div class=\"fof\"><h1>Error 404</h1></div><style>*{transition:all .6s}html{height:100%}body{font-family:Lato,sans-serif;color:#888;margin:0}#main{display:table;width:100%;height:100vh;text-align:center}.fof{display:table-cell;vertical-align:middle}.fof h1{font-size:50px;display:inline-block;padding-right:12px;animation:type .5s alternate infinite}@keyframes type{from{box-shadow:inset -3px 0 0 #888}to{box-shadow:inset -3px 0 0 transparent}}</style></div>";
 
     public static WebServerManager getInstance() {
         if (instance == null) {
@@ -97,6 +98,4 @@ public class WebServerManager implements HttpHandler {
     public static String getError404() {
         return error404;
     }
-
-    private static final String error404 = "<div id=\"main\"><div class=\"fof\"><h1>Error 404</h1></div><style>*{transition:all .6s}html{height:100%}body{font-family:Lato,sans-serif;color:#888;margin:0}#main{display:table;width:100%;height:100vh;text-align:center}.fof{display:table-cell;vertical-align:middle}.fof h1{font-size:50px;display:inline-block;padding-right:12px;animation:type .5s alternate infinite}@keyframes type{from{box-shadow:inset -3px 0 0 #888}to{box-shadow:inset -3px 0 0 transparent}}</style></div>";
 }
