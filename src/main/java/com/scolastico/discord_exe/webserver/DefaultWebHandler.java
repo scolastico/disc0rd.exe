7245bb461eecfb0ff3326304d9f83a54f1c9ecd7
Joschua Becker <joschuabeckerprivat@gmail.com>
2020-01-29 20:58:13 +0100

    hot fix default web handler

--- src/main/java/com/scolastico/discord_exe/webserver/DefaultWebHandler.java
+++ src/main/java/com/scolastico/discord_exe/webserver/DefaultWebHandler.java
@@ -3,17 +3,19 @@ package com.scolastico.discord_exe.webserver;
 import com.scolastico.discord_exe.Disc0rd;
 import com.scolastico.discord_exe.etc.ErrorHandler;
 import com.sun.net.httpserver.HttpExchange;
+import org.reflections.Reflections;
+import org.reflections.scanners.ResourcesScanner;
 
 import java.io.*;
 import java.net.URLConnection;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Set;
 
 public class DefaultWebHandler {
 
     private static DefaultWebHandler instance = null;
     private HashMap<String, String> paths = new HashMap<>();
-    private ArrayList<String> filesRaw = new ArrayList<>();
 
     public static DefaultWebHandler getInstance() {
         if (instance == null) {
@@ -23,23 +25,14 @@ public class DefaultWebHandler {
     }
 
     private DefaultWebHandler() {
-        getResourceFilesRecursive("/webServer/");
-        for (String path:filesRaw) {
+        Reflections reflections = new Reflections("webServer", new ResourcesScanner());
+        Set<String> resourceList = reflections.getResources(x -> true);
+        ArrayList<String> filesRaw = new ArrayList<>();
+        for (String path:resourceList) filesRaw.add("/" + path);
+        for (String path: filesRaw) {
             this.paths.put(path.replaceFirst("/webServer", ""), path);
             if (path.endsWith("/index.html")) {
                 this.paths.put(path.replaceFirst("/webServer", "").substring(0, path.length() - 20), path);
-            }
-        }
-    }
-
-    private void getResourceFilesRecursive(String path) {
-        ArrayList<String> filePaths = getResourceFiles(path);
-        for (String filePath:filePaths) {
-            File file = new File(getClass().getResource(path + filePath).getPath());
-            if (file.isDirectory()) {
-                getResourceFilesRecursive(path + filePath + "/");
-            } else {
-                filesRaw.add(path + filePath);
             }
         }
     }
@@ -76,34 +69,6 @@ public class DefaultWebHandler {
         } catch (Exception e) {
             ErrorHandler.getInstance().handle(e);
         }
-    }
-
-    private ArrayList<String> getResourceFiles(String path) {
-        try {
-            ArrayList<String> filenames = new ArrayList<>();
-            try (
-                    InputStream in = getResourceAsStream(path);
-                    BufferedReader br = new BufferedReader(new InputStreamReader(in))
-            ) {
-                String resource;
-                while ((resource = br.readLine()) != null) {
-                    filenames.add(resource);
-                }
-            }
-            return filenames;
-        } catch (Exception e) {
-            ErrorHandler.getInstance().handleFatal(e);
-        }
-        return new ArrayList<>();
-    }
-
-    private InputStream getResourceAsStream(String resource) {
-        final InputStream in = getContextClassLoader().getResourceAsStream(resource);
-        return in == null ? getClass().getResourceAsStream(resource) : in;
-    }
-
-    private ClassLoader getContextClassLoader() {
-        return Thread.currentThread().getContextClassLoader();
     }
 
 }
