f79905b5ce90adc81c7590185c4ff62e13a43b0b
Joschua Becker <joschuabeckerprivat@gmail.com>
2020-03-25 22:10:53 +0100

    added getPr0grammPost function to Pr0gramm API

--- src/main/java/com/scolastico/discord_exe/etc/Pr0grammAPI.java
+++ src/main/java/com/scolastico/discord_exe/etc/Pr0grammAPI.java
@@ -2,7 +2,6 @@ package com.scolastico.discord_exe.etc;
 
 
 import org.apache.http.Header;
-import org.apache.http.HttpEntity;
 import org.apache.http.HttpHeaders;
 import org.apache.http.NameValuePair;
 import org.apache.http.client.entity.UrlEncodedFormEntity;
@@ -13,9 +12,13 @@ import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
 import org.apache.http.message.BasicNameValuePair;
 import org.apache.http.util.EntityUtils;
+import org.json.JSONArray;
 import org.json.JSONObject;
 
 import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -29,8 +32,12 @@ public class Pr0grammAPI {
 		<artifactId>httpclient</artifactId>
 		<version>4.5.10</version>
 	</dependency>
+	<dependency>
+        <groupId>org.json</groupId>
+        <artifactId>json</artifactId>
+        <version>20190722</version>
+    </dependency>
      */
-
 
     private static final String pr0_url = "https://pr0gramm.com/api/";
     private final CloseableHttpClient httpClient = HttpClients.createDefault();
@@ -103,6 +110,60 @@ public class Pr0grammAPI {
         httpClient.close();
     }
 
+    public Pr0grammGetItemsRequestGenerator generateGetItemsRequestGenerator() {
+        return new Pr0grammGetItemsRequestGenerator(this);
+    }
+
+    public Pr0grammPost[] getPr0grammPosts(Pr0grammGetItemsRequestGenerator generator) throws Pr0grammApiError {
+        try {
+            ArrayList<Pr0grammPost> ret = new ArrayList<>();
+            String request = makeGetRequest(generator.generate());
+            if (request == null) {
+                throw new Pr0grammApiError("response is null");
+            }
+            JSONObject object = new JSONObject(request);
+            if (object.has("error")) {
+                if (object.get("error") instanceof String) {
+                    throw new Pr0grammApiError(object.getString("error"));
+                }
+            }
+            if (!object.has("items")) throw new Pr0grammApiError("response not valid");
+            JSONArray array = object.getJSONArray("items");
+            for (int index = 0; index != array.length(); index++) {
+                JSONObject entry = array.getJSONObject(index);
+                ret.add(new Pr0grammPost(
+                        entry.getLong("id"),
+                        entry.getInt("promoted") != 0,
+                        entry.getLong("userId"),
+                        entry.getInt("up"),
+                        entry.getInt("down"),
+                        entry.getLong("created"),
+                        entry.getString("image").toUpperCase().endsWith(".PNG") ||
+                                entry.getString("image").toUpperCase().endsWith(".JPEG") ||
+                                entry.getString("image").toUpperCase().endsWith(".JPG") ||
+                                entry.getString("image").toUpperCase().endsWith(".GIF") ?
+                                "https://img.pr0gramm.com/" + entry.getString("image") :
+                                "https://vid.pr0gramm.com/" + entry.getString("image"),
+                        "https://thumb.pr0gramm.com/" + entry.getString("thumb"),
+                        entry.getString("fullsize").isEmpty() ? null : "https://full.pr0gramm.com/" + entry.getString("fullsize"),
+                        entry.getInt("width"),
+                        entry.getInt("height"),
+                        entry.getBoolean("audio"),
+                        entry.getString("source"),
+                        entry.getInt("flags"),
+                        entry.getString("user"),
+                        entry.getInt("mark"),
+                        entry.getInt("gift")
+                ));
+            }
+            return ret.toArray(new Pr0grammPost[0]);
+        } catch (Pr0grammApiError e) {
+            throw e;
+        } catch (Exception e) {
+            throw new Pr0grammApiError("Unknown Error: " + e.getMessage());
+        }
+    }
+
     public static class Pr0grammLoginError extends Exception {
         private int type = 0;
 
@@ -127,6 +188,381 @@ public class Pr0grammAPI {
 
         public Pr0grammApiError(String message) {
             super(message);
+        }
+    }
+
+    public static class Pr0grammPost {
+        private long id;
+        private boolean promoted;
+        private long userId;
+        private int up;
+        private int down;
+        private long created;
+        private String image;
+        private String thumbnail;
+        private String full;
+        private int width;
+        private int height;
+        private boolean audio;
+        private String source;
+        private int flags;
+        private String user;
+        private int mark;
+        private int gift;
+
+        private Pr0grammPost(long id, boolean promoted, long userId, int up, int down, long created, String image, String thumbnail, String full, int width, int height, boolean audio, String source, int flags, String user, int mark, int gift) {
+            this.id = id;
+            this.promoted = promoted;
+            this.userId = userId;
+            this.up = up;
+            this.down = down;
+            this.created = created;
+            this.image = image;
+            this.thumbnail = thumbnail;
+            this.full = full;
+            this.width = width;
+            this.height = height;
+            this.audio = audio;
+            this.source = source;
+            this.flags = flags;
+            this.user = user;
+            this.mark = mark;
+            this.gift = gift;
+        }
+
+        public long getId() {
+            return id;
+        }
+
+        public boolean isPromoted() {
+            return promoted;
+        }
+
+        public long getUserId() {
+            return userId;
+        }
+
+        public int getUp() {
+            return up;
+        }
+
+        public int getDown() {
+            return down;
+        }
+
+        public long getCreated() {
+            return created;
+        }
+
+        public String getImage() {
+            return image;
+        }
+
+        public String getThumbnail() {
+            return thumbnail;
+        }
+
+        public String getFull() {
+            return full;
+        }
+
+        public int getWidth() {
+            return width;
+        }
+
+        public int getHeight() {
+            return height;
+        }
+
+        public boolean isAudio() {
+            return audio;
+        }
+
+        public String getSource() {
+            return source;
+        }
+
+        public int getFlags() {
+            return flags;
+        }
+
+        public String getUser() {
+            return user;
+        }
+
+        public int getMark() {
+            return mark;
+        }
+
+        public int getGift() {
+            return gift;
+        }
+    }
+
+    public static class Pr0grammFlagCalculator {
+        boolean sfw = true;
+        boolean nsfw = false;
+        boolean nsfl = false;
+
+        public boolean isSfw() {
+            return sfw;
+        }
+
+        public void setSfw(boolean sfw) {
+            this.sfw = sfw;
+        }
+
+        public boolean isNsfw() {
+            return nsfw;
+        }
+
+        public void setNsfw(boolean nsfw) {
+            this.nsfw = nsfw;
+        }
+
+        public boolean isNsfl() {
+            return nsfl;
+        }
+
+        public void setNsfl(boolean nsfl) {
+            this.nsfl = nsfl;
+        }
+
+        public int calculate() {
+            int ret = 0;
+            if (sfw) ret += 9;
+            if (nsfw) ret += 2;
+            if (nsfl) ret += 4;
+            if (ret == 0) ret = 9;
+            return ret;
+        }
+    }
+
+    public static class Pr0grammUser {
+        private long id;
+        private String name;
+        private long registered;
+        private long score;
+        private int admin;
+        private long banned;
+        private long inactive;
+        private long commentsDeleted;
+        private long itemsDeleted;
+        private long commentCount;
+        private long commentLikesCount;
+        private long uploadCount;
+        private boolean likesArePublic;
+        private long likesCount;
+        private long tagCount;
+        private badge[] badges;
+        private int mark;
+
+        public Pr0grammUser(long id, String name, long registered, long score, int admin, long banned, long inactive, long commentsDeleted, long itemsDeleted, long commentCount, long commentLikesCount, long uploadCount, boolean likesArePublic, long likesCount, long tagCount, badge[] badges, int mark) {
+            this.id = id;
+            this.name = name;
+            this.registered = registered;
+            this.score = score;
+            this.admin = admin;
+            this.banned = banned;
+            this.inactive = inactive;
+            this.commentsDeleted = commentsDeleted;
+            this.itemsDeleted = itemsDeleted;
+            this.commentCount = commentCount;
+            this.commentLikesCount = commentLikesCount;
+            this.uploadCount = uploadCount;
+            this.likesArePublic = likesArePublic;
+            this.likesCount = likesCount;
+            this.tagCount = tagCount;
+            this.badges = badges;
+            this.mark = mark;
+        }
+
+        public long getId() {
+            return id;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public long getRegistered() {
+            return registered;
+        }
+
+        public long getScore() {
+            return score;
+        }
+
+        public int getAdmin() {
+            return admin;
+        }
+
+        public long getBanned() {
+            return banned;
+        }
+
+        public long getInactive() {
+            return inactive;
+        }
+
+        public long getCommentsDeleted() {
+            return commentsDeleted;
+        }
+
+        public long getItemsDeleted() {
+            return itemsDeleted;
+        }
+
+        public long getCommentCount() {
+            return commentCount;
+        }
+
+        public long getCommentLikesCount() {
+            return commentLikesCount;
+        }
+
+        public long getUploadCount() {
+            return uploadCount;
+        }
+
+        public boolean isLikesArePublic() {
+            return likesArePublic;
+        }
+
+        public long getLikesCount() {
+            return likesCount;
+        }
+
+        public long getTagCount() {
+            return tagCount;
+        }
+
+        public badge[] getBadges() {
+            return badges;
+        }
+
+        public int getMark() {
+            return mark;
+        }
+
+        public static class badge {
+            private String link;
+            private String image;
+            private String description;
+            private long created;
+
+            private badge(String link, String image, String description, long created) {
+                this.link = link;
+                this.image = image;
+                this.description = description;
+                this.created = created;
+            }
+
+            public String getLink() {
+                return link;
+            }
+
+            public String getImage() {
+                return image;
+            }
+
+            public String getDescription() {
+                return description;
+            }
+
+            public long getCreated() {
+                return created;
+            }
+        }
+    }
+
+    public static class Pr0grammGetItemsRequestGenerator {
+        Pr0grammAPI api;
+
+        private Long older = null;
+        private Pr0grammFlagCalculator flagCalculator = null;
+        private boolean promoted = false;
+        private String user = null;
+        private String likes = null;
+        private String tags = null;
+        private Long newer =  null;
+
+        private Pr0grammGetItemsRequestGenerator(Pr0grammAPI api) {
+            this.api = api;
+        }
+
+        private String generate() {
+            String ret = "";
+            if (older != null) ret += "&older=" + older.toString();
+            if (newer != null) ret += "&newer=" + newer.toString();
+            if (flagCalculator != null) ret += "&flags=" + flagCalculator.calculate();
+            if (promoted) ret += "&promoted=1"; else ret += "&promoted=0";
+            if (user != null) ret += "&user=" + user;
+            if (likes != null) ret += "&likes=" + likes;
+            try {
+                if (tags != null) ret += "&tags=" + URLEncoder.encode(tags, StandardCharsets.UTF_8.toString());
+            } catch (UnsupportedEncodingException ignored) {}
+            return "items/get?" + ret.substring(1);
+        }
+
+        public Pr0grammAPI getApi() {
+            return api;
+        }
+
+        public Long getOlder() {
+            return older;
+        }
+
+        public void setOlder(Long older) {
+            this.older = older;
+        }
+
+        public void setUser(String user) {
+            this.user = user;
+        }
+
+        public void setLikes(String likes) {
+            this.likes = likes;
+        }
+
+        public void setTags(String tags) {
+            this.tags = tags;
+        }
+
+        public void setNewer(Long newer) {
+            this.newer = newer;
+        }
+
+        public void setFlagCalculator(Pr0grammFlagCalculator flagCalculator) {
+            this.flagCalculator = flagCalculator;
+        }
+
+        public void setPromoted(boolean promoted) {
+            this.promoted = promoted;
+        }
+
+        public Pr0grammFlagCalculator getFlagCalculator() {
+            return flagCalculator;
+        }
+
+        public boolean isPromoted() {
+            return promoted;
+        }
+
+        public String getUser() {
+            return user;
+        }
+
+        public String getLikes() {
+            return likes;
+        }
+
+        public String getTags() {
+            return tags;
+        }
+
+        public Long getNewer() {
+            return newer;
         }
     }
 
