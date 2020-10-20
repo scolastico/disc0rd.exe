cec91a2e000dbf7d0cc39730ef09b3e5c4321b5f
Restyled.io <commits@restyled.io>
2020-10-20 07:07:24 +0000

    Restyled by prettier

--- src/main/resources/webServer/owner-panel/assets/js/settings.main.js
+++ src/main/resources/webServer/owner-panel/assets/js/settings.main.js
@@ -1,62 +1,68 @@
 function noOtpAuth() {
   const Toast = Swal.mixin({
-    toast : true,
-    position : "top-end",
-    showConfirmButton : false,
-    timerProgressBar : true,
-    onOpen : (toast) => {
+    toast: true,
+    position: "top-end",
+    showConfirmButton: false,
+    timerProgressBar: true,
+    onOpen: (toast) => {
       toast.addEventListener("mouseenter", Swal.stopTimer);
       toast.addEventListener("mouseleave", Swal.resumeTimer);
-    }
+    },
   });
-  Toast.fire({type : "error", title : "OTP Not Valid!"});
+  Toast.fire({ type: "error", title: "OTP Not Valid!" });
 }
 
 function beautifyJsonString(jsonString) {
   try {
     return JSON.stringify(JSON.parse(jsonString), null, 4);
-  } catch (e) {
-  }
+  } catch (e) {}
   return jsonString;
 }
 
 function saveConfig() {
-  if (document.getElementById("config").value === "" ||
-      document.getElementById("otp").value === "") {
+  if (
+    document.getElementById("config").value === "" ||
+    document.getElementById("otp").value === ""
+  ) {
     swal.fire({
-      type : "error",
-      title : "Oops...",
-      text : "Please enter a value in all fields!"
+      type: "error",
+      title: "Oops...",
+      text: "Please enter a value in all fields!",
     });
     return;
   }
   const xmlHttpSettingsSend = new XMLHttpRequest();
-  xmlHttpSettingsSend.open("POST",
-                           "../api/v1/admin/saveConfig/" +
-                               document.getElementById("otp").value,
-                           true);
+  xmlHttpSettingsSend.open(
+    "POST",
+    "../api/v1/admin/saveConfig/" + document.getElementById("otp").value,
+    true
+  );
   document.getElementById("otp").value = "";
   xmlHttpSettingsSend.setRequestHeader("Content-type", "application/json");
-  xmlHttpSettingsSend.onreadystatechange = function() {
-    if (xmlHttpSettingsSend.readyState === 4 &&
-        xmlHttpSettingsSend.status === 200) {
+  xmlHttpSettingsSend.onreadystatechange = function () {
+    if (
+      xmlHttpSettingsSend.readyState === 4 &&
+      xmlHttpSettingsSend.status === 200
+    ) {
       if (JSON.parse(xmlHttpSettingsSend.responseText).status === "ok") {
         const Toast = Swal.mixin({
-          toast : true,
-          position : "top-end",
-          showConfirmButton : false,
-          timer : 3000,
-          onOpen : (toast) => {
+          toast: true,
+          position: "top-end",
+          showConfirmButton: false,
+          timer: 3000,
+          onOpen: (toast) => {
             toast.addEventListener("mouseenter", Swal.stopTimer);
             toast.addEventListener("mouseleave", Swal.resumeTimer);
-          }
+          },
         });
-        Toast.fire({type : "success", title : "Saved!"});
+        Toast.fire({ type: "success", title: "Saved!" });
         return;
-      } else if (JSON.parse(xmlHttpSettingsSend.responseText).status ===
-                 "error") {
-        if (JSON.parse(xmlHttpSettingsSend.responseText).error ===
-            "no otp auth") {
+      } else if (
+        JSON.parse(xmlHttpSettingsSend.responseText).status === "error"
+      ) {
+        if (
+          JSON.parse(xmlHttpSettingsSend.responseText).error === "no otp auth"
+        ) {
           noOtpAuth();
           return;
         }
@@ -70,42 +76,47 @@ function saveConfig() {
 function loadConfig() {
   if (document.getElementById("otp").value === "") {
     swal.fire({
-      type : "error",
-      title : "Oops...",
-      text : "Please enter the Google Authenticator code!"
+      type: "error",
+      title: "Oops...",
+      text: "Please enter the Google Authenticator code!",
     });
     return;
   }
   const xmlHttpSettingsSend = new XMLHttpRequest();
-  xmlHttpSettingsSend.open("POST",
-                           "../api/v1/admin/getConfig/" +
-                               document.getElementById("otp").value,
-                           true);
+  xmlHttpSettingsSend.open(
+    "POST",
+    "../api/v1/admin/getConfig/" + document.getElementById("otp").value,
+    true
+  );
   document.getElementById("otp").value = "";
-  xmlHttpSettingsSend.onreadystatechange = function() {
-    if (xmlHttpSettingsSend.readyState === 4 &&
-        xmlHttpSettingsSend.status === 200) {
+  xmlHttpSettingsSend.onreadystatechange = function () {
+    if (
+      xmlHttpSettingsSend.readyState === 4 &&
+      xmlHttpSettingsSend.status === 200
+    ) {
       if (JSON.parse(xmlHttpSettingsSend.responseText).status === "ok") {
-        document.getElementById("config").value =
-            beautifyJsonString(JSON.stringify(
-                JSON.parse(xmlHttpSettingsSend.responseText).config));
+        document.getElementById("config").value = beautifyJsonString(
+          JSON.stringify(JSON.parse(xmlHttpSettingsSend.responseText).config)
+        );
         autosize(document.getElementById("config"));
         const Toast = Swal.mixin({
-          toast : true,
-          position : "top-end",
-          showConfirmButton : false,
-          timer : 3000,
-          onOpen : (toast) => {
+          toast: true,
+          position: "top-end",
+          showConfirmButton: false,
+          timer: 3000,
+          onOpen: (toast) => {
             toast.addEventListener("mouseenter", Swal.stopTimer);
             toast.addEventListener("mouseleave", Swal.resumeTimer);
-          }
+          },
         });
-        Toast.fire({type : "success", title : "Loaded!"});
+        Toast.fire({ type: "success", title: "Loaded!" });
         return;
-      } else if (JSON.parse(xmlHttpSettingsSend.responseText).status ===
-                 "error") {
-        if (JSON.parse(xmlHttpSettingsSend.responseText).error ===
-            "no otp auth") {
+      } else if (
+        JSON.parse(xmlHttpSettingsSend.responseText).status === "error"
+      ) {
+        if (
+          JSON.parse(xmlHttpSettingsSend.responseText).error === "no otp auth"
+        ) {
           noOtpAuth();
           return;
         }
@@ -119,53 +130,60 @@ function loadConfig() {
 function sendSettings() {
   if (document.getElementById("w2gDefaultPlayback").value === "") {
     swal.fire({
-      type : "error",
-      title : "Oops...",
-      text : "Please enter a value in all fields!"
+      type: "error",
+      title: "Oops...",
+      text: "Please enter a value in all fields!",
     });
     return;
   }
   const xmlHttpSettingsSend = new XMLHttpRequest();
   xmlHttpSettingsSend.open("POST", "../api/v1/admin/saveSettings", true);
-  xmlHttpSettingsSend.setRequestHeader("Content-type",
-                                       "application/x-www-form-urlencoded");
-  xmlHttpSettingsSend.onreadystatechange = function() {
-    if (xmlHttpSettingsSend.readyState === 4 &&
-        xmlHttpSettingsSend.status === 200) {
+  xmlHttpSettingsSend.setRequestHeader(
+    "Content-type",
+    "application/x-www-form-urlencoded"
+  );
+  xmlHttpSettingsSend.onreadystatechange = function () {
+    if (
+      xmlHttpSettingsSend.readyState === 4 &&
+      xmlHttpSettingsSend.status === 200
+    ) {
       if (JSON.parse(xmlHttpSettingsSend.responseText).status === "ok") {
         const Toast = Swal.mixin({
-          toast : true,
-          position : "top-end",
-          showConfirmButton : false,
-          timer : 3000,
-          onOpen : (toast) => {
+          toast: true,
+          position: "top-end",
+          showConfirmButton: false,
+          timer: 3000,
+          onOpen: (toast) => {
             toast.addEventListener("mouseenter", Swal.stopTimer);
             toast.addEventListener("mouseleave", Swal.resumeTimer);
-          }
+          },
         });
-        Toast.fire({type : "success", title : "Saved!"});
+        Toast.fire({ type: "success", title: "Saved!" });
       } else {
         window.location.replace("logout.html");
       }
     }
   };
   xmlHttpSettingsSend.send(
-      "w2gDefaultPlayback=" +
-      encodeURIComponent(document.getElementById("w2gDefaultPlayback").value));
+    "w2gDefaultPlayback=" +
+      encodeURIComponent(document.getElementById("w2gDefaultPlayback").value)
+  );
 }
 
 const xmlHttpSettings = new XMLHttpRequest();
 xmlHttpSettings.open("GET", "../api/v1/admin/getSettings", true);
-xmlHttpSettings.onreadystatechange = function() {
+xmlHttpSettings.onreadystatechange = function () {
   if (xmlHttpSettings.readyState === 4 && xmlHttpSettings.status === 200) {
     if (JSON.parse(xmlHttpSettings.responseText).status === "ok") {
-      document.getElementById("w2gDefaultPlayback").value =
-          JSON.parse(xmlHttpSettings.responseText).settings.w2gDefaultPlayback;
+      document.getElementById("w2gDefaultPlayback").value = JSON.parse(
+        xmlHttpSettings.responseText
+      ).settings.w2gDefaultPlayback;
     } else {
       window.location.replace("logout.html");
     }
   }
 };
 xmlHttpSettings.send(
-    "w2gDefaultPlayback=" +
-    encodeURIComponent(document.getElementById("w2gDefaultPlayback").value));
+  "w2gDefaultPlayback=" +
+    encodeURIComponent(document.getElementById("w2gDefaultPlayback").value)
+);
